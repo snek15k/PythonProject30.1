9cda04aba3d86af1d7b7143a1e6d5cf3d74dc59d
artem <artemignatuk78@gmail.com>
2025-07-08 19:31:38 +0500

    Commit_3

--- config/settings.py
+++ config/settings.py
@@ -134,3 +134,8 @@ STATIC_URL = 'static/'
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 AUTH_USER_MODEL = 'users.User'
+
+
+MEDIA_URL = '/media/'
+
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
