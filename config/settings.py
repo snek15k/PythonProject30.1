bbfb422adbc17fe0957a16f235755d7d57fd94af
artem <artemignatuk78@gmail.com>
2025-09-18 13:29:51 +0500

    Commit_75

--- config/settings.py
+++ config/settings.py
@@ -32,7 +32,7 @@ SECRET_KEY = os.getenv('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True if os.getenv('DEBUG') == "True" else False
 
-ALLOWED_HOSTS = ["130.193.41.41", "web", "django"]
+ALLOWED_HOSTS = ["130.193.41.41", "backend", "django"]
 
 
 # Application definition
